23eaef4268acfe2b0cbdbbc3cc1a62b1d255d3de
NiTiSon <NiTiSon@users.noreply.github.com>
2023-06-20 23:05:12 +0300

    JoystickId

---
+++ src/NiTiS.Native.SDL3/SdlJoystickId.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace NiTiS.Native.SDL3;
+
+public readonly struct SdlJoystickId
+{
+	private readonly uint __value;
+
+	public static SdlJoystickId Nil => default;
+
+	public static implicit operator uint(SdlJoystickId id)
+		=> id.__value;
+
+	public static implicit operator SdlJoystickId(uint value)
+		=> Unsafe.As<uint, SdlJoystickId>(ref value);
+
+	public static explicit operator int(SdlJoystickId id)
+		=> (int)id.__value;
+
+	public static explicit operator SdlJoystickId(int value)
+		=> Unsafe.As<int, SdlJoystickId>(ref value);
+
+	/// <inheritdoc/>
+	public override string ToString()
+		=> $"Joystick {{ Api: SDL, Id: {(ulong)__value}}}";
+}
